7d49017afa91d1b0648a69c62dea5ae6c19b2874
FurqanMujahid67 <your-email@example.com>
2026-02-22 03:42:31 -0800

    json ajsut

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,35 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'home',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'shop',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'about',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'contact',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'theonlykingonearthisasad/testlaoding',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'shop-details/:idOrUuid',
+    renderMode: RenderMode.Client,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Client,
+  },
 ];
